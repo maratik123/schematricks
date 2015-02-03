a056a69700ad9ff2bf4d9cfd7c20f10376ef696c
Marat Bukharov <bukharov@bpcbt.com>
2015-02-03 18:33:43 +0300

    Add TLS-backed lines buffer

--- gridscene.cpp
+++ gridscene.cpp
@@ -2,12 +2,16 @@
 #include <QPainter>
 #include <cmath>
 
+namespace {
+    thread_local QVarLengthArray<QLineF> lines;
+}
+
 void GridScene::drawBackground(QPainter *painter, const QRectF &rect)
 {
     qreal left =  rect.left() - std::remainder(rect.left(), _gridSize);
     qreal top =  rect.top() - std::remainder(rect.top(), _gridSize);
 
-    QVarLengthArray<QLineF> lines;
+    lines.clear();
 
     for(qreal x = left; x < rect.right(); x += _gridSize)
         lines.append(QLineF(x, rect.top(), x, rect.bottom()));
