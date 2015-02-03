a8f1925ae677f3d30a87343c1f278405aae4db17
Marat Bukharov <bukharov@bpcbt.com>
2015-02-03 14:56:22 +0300

    Finish with draw grid

--- gridscene.cpp
+++ gridscene.cpp
@@ -1,4 +1,5 @@
 #include "gridscene.h"
+#include <QPainter>
 
 void GridScene::drawBackground(QPainter *painter, const QRectF &rect)
 {
@@ -7,4 +8,13 @@ void GridScene::drawBackground(QPainter *painter, const QRectF &rect)
     int itop = static_cast<int>(rect.top());
     qreal left =  ileft - ileft % gridSize;
     qreal top =  itop - itop % gridSize;
+
+    QVarLengthArray<QLineF> lines;
+
+    for(qreal x = left; x < rect.right(); x += gridSize)
+        lines.append(QLineF(x, rect.top(), x, rect.bottom()));
+    for(qreal y = top; y < rect.bottom(); y += gridSize)
+        lines.append(QLineF(rect.left(), y, rect.right(), y));
+
+    painter->drawLines(lines.constData(), lines.size());
 }
