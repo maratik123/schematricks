958dcb94f2c6f57f3494eb15ac4a3fb1dff578c6
Marat Bukharov <bukharov@bpcbt.com>
2015-02-04 09:49:05 +0300

    Revert "Add TLS-backed lines buffer"
    This reverts commit a056a69700ad9ff2bf4d9cfd7c20f10376ef696c.

--- gridscene.cpp
+++ gridscene.cpp
@@ -2,16 +2,12 @@
 #include <QPainter>
 #include <cmath>
 
-namespace {
-    thread_local QVarLengthArray<QLineF> lines;
-}
-
 void GridScene::drawBackground(QPainter *painter, const QRectF &rect)
 {
     qreal left =  rect.left() - std::remainder(rect.left(), _gridSize);
     qreal top =  rect.top() - std::remainder(rect.top(), _gridSize);
 
-    lines.clear();
+    QVarLengthArray<QLineF> lines;
 
     for(qreal x = left; x < rect.right(); x += _gridSize)
         lines.append(QLineF(x, rect.top(), x, rect.bottom()));
