6f38b4d46116b903b3a3a22d5f30f385e0443d39
Marat Bukharov <bukharov@bpcbt.com>
2015-02-06 18:33:07 +0300

    Make AbstractSchemaItem discrete

--- abstractschemaitem.cpp
+++ abstractschemaitem.cpp
@@ -1,9 +1,45 @@
 #include "abstractschemaitem.h"
 #include "utils.h"
 
-
 AbstractSchemaItem::AbstractSchemaItem(int i, int j, qreal cellSize, QGraphicsItem *parent)
-    : QGraphicsRectItem(parent), _cellSize(cellSize), _i(i), _j(j)
+    : QAbstractGraphicsShapeItem(parent), _cellSize(cellSize)
 {
-    setRect(QRectF(i * cellSize, j * cellSize, cellSize, cellSize));
+    setGridPos(i, j);
+}
+
+AbstractSchemaItem::AbstractSchemaItem(const QPoint &gridPos, qreal cellSize, QGraphicsItem *parent)
+    : QAbstractGraphicsShapeItem(parent), _cellSize(cellSize)
+{
+    setGridPos(gridPos);
+}
+
+void AbstractSchemaItem::setI(int i)
+{
+    auto checker = [this, i] { return _gridPos.x() == i; };
+    auto updater = [this, i]() mutable { _gridPos.setX(i); };
+    setGridPos(checker, updater);
+}
+
+void AbstractSchemaItem::setJ(int j)
+{
+    auto checker = [this, j] { return _gridPos.y() == j; };
+    auto updater = [this, j]() mutable { _gridPos.setY(j); };
+    setGridPos(checker, updater);
+}
+
+void AbstractSchemaItem::setGridPos(const QPoint &gridPos)
+{
+    auto checker = [this, &gridPos] { return _gridPos == gridPos; };
+    auto updater = [this, &gridPos]() mutable { _gridPos = gridPos; };
+    setGridPos(checker, updater);
+}
+
+template<class Checker, class Updater>
+void AbstractSchemaItem::setGridPos(Checker checker, Updater updater)
+{
+    if(checker())
+        return;
+    prepareGeometryChange();
+    updater();
+    update();
 }
--- abstractschemaitem.h
+++ abstractschemaitem.h
@@ -1,29 +1,41 @@
 #ifndef ABSTRACTSCHEMAITEM_H
 #define ABSTRACTSCHEMAITEM_H
 
-#include <QGraphicsItem>
+#include <QAbstractGraphicsShapeItem>
 
 enum class SchemaItemType;
 
-class AbstractSchemaItem : public QGraphicsItem
+class AbstractSchemaItem : public QAbstractGraphicsShapeItem
 {
     Q_DISABLE_COPY(AbstractSchemaItem)
 
 public:
     explicit AbstractSchemaItem(int i, int j, qreal cellSize, QGraphicsItem *parent = nullptr);
+    explicit AbstractSchemaItem(const QPoint &gridPos, qreal cellSize, QGraphicsItem *parent = nullptr);
     qreal gridSize() const { return _cellSize; }
 
     int type() const { return static_cast<int>(schemaItemType()); }
-    int i() const { return _i; }
-    int j() const { return _j; }
-    void setI(int i) { _i = i; setX(i * _cellSize); }
-    void setJ(int j) { _j = j; setY(j * _cellSize); }
+    int i() const { return _gridPos.x(); }
+    int j() const { return _gridPos.y(); }
+    const QPoint &gridPos() const { return _gridPos; }
+    void setI(int i);
+    void setJ(int j);
+    void setGridPos(const QPoint &gridPos);
+    void setGridPos(int i, int j) { setGridPos(QPoint(i, j)); }
+
     virtual SchemaItemType schemaItemType() const = 0;
 
+    QRectF boundingRect() const { return QRectF(QPointF(_gridPos) * _cellSize, QSizeF(_cellSize, _cellSize)); }
+
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = 0) = 0;
+
 private:
+    void updateBoundingRect();
+    template<class Checker, class Updater>
+    void setGridPos(Checker checker, Updater updater);
+
     const qreal _cellSize;
-    int _i;
-    int _j;
+    QPoint _gridPos;
 };
 
 #endif // ABSTRACTSCHEMAITEM_H
