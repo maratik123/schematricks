5089d6e5f8159cf3bbc28045d97afa29fda0529b
Marat Bukharov <bukharov@bpcbt.com>
2015-02-06 18:35:17 +0300

    Make MetalSchemaItem viable.

--- metalschemaitem.cpp
+++ metalschemaitem.cpp
@@ -1,12 +1,27 @@
 #include "metalschemaitem.h"
 
-MetalSchemaItem::MetalSchemaItem()
-{
+namespace {
+class ColorDef {
+    QColor color;
 
+public:
+    ColorDef() : color(Qt::gray) {
+        color.setAlphaF(0.7);
+    }
+    const QColor &getColor() const { return color; }
+};
+
+const static ColorDef colorDef;
 }
 
-MetalSchemaItem::~MetalSchemaItem()
+MetalSchemaItem::MetalSchemaItem(int i, int j, qreal cellSize, QGraphicsItem *parent)
+    : LayeredSchemaItem(colorDef.getColor(), i, j, cellSize, parent)
 {
-
+    setZValue(10);
 }
 
+MetalSchemaItem::MetalSchemaItem(const QPoint &gridPos, qreal cellSize, QGraphicsItem *parent)
+    : LayeredSchemaItem(colorDef.getColor(), gridPos, cellSize, parent)
+{
+    setZValue(10);
+}
--- metalschemaitem.h
+++ metalschemaitem.h
@@ -1,14 +1,16 @@
 #ifndef METALSCHEMAITEM_H
 #define METALSCHEMAITEM_H
 
-#include "abstractschemaitem.h"
+#include "layeredschemaitem.h"
 #include "schemaitemtyperegistry.h"
 
-class MetalSchemaItem : public AbstractSchemaItem
+class MetalSchemaItem : public LayeredSchemaItem
 {
 public:
-    explicit MetalSchemaItem(int i, int j, qreal cellSize, QGraphicsItem *parent = nullptr) : AbstractSchemaItem(i, j, cellSize, parent) {}
+    explicit MetalSchemaItem(int i, int j, qreal cellSize, QGraphicsItem *parent = nullptr);
+    explicit MetalSchemaItem(const QPoint &gridPos, qreal cellSize, QGraphicsItem *parent = nullptr);
 
+    SchemaItemType schemaItemType() const { return SchemaItemType::Metal; }
 };
 
 #endif // METALSCHEMAITEM_H
