ad5eac286edf70228ddec18e80acb4aac81716bc
Marat Buharov <marat.buharov@gmail.com>
2015-02-12 14:42:29 +0300

    Rework on LayeredColorHelper

--- layeredcolorhelper.cpp
+++ layeredcolorhelper.cpp
@@ -1,10 +1,20 @@
 #include "layeredcolorhelper.h"
 
-void LayeredColorHelper::setOutline(const QColor &outlineColor)
-{
+template<class ColorType>
+void LayeredColorHelper::setOutlineGeneric(const ColorType &outlineColor) {
     QColor _outlineColor = outlineColor;
     _outlineColor.setAlphaF(0.7);
     _outline = _outlineColor;
+}
+
+void LayeredColorHelper::setOutline(const QColor &outlineColor)
+{
+    setOutlineGeneric(outlineColor);
+}
+
+void LayeredColorHelper::setOutline(Qt::GlobalColor outlineColor)
+{
+    setOutlineGeneric(outlineColor);
 }
 
 void LayeredColorHelper::setBrush(const QColor &innerColor)
--- layeredcolorhelper.h
+++ layeredcolorhelper.h
@@ -14,10 +14,16 @@ public:
     explicit LayeredColorHelper(Qt::GlobalColor innerColor, Qt::GlobalColor outlineColor) { setOutline(outlineColor); setBrush(innerColor); }
     explicit LayeredColorHelper(const QBrush &innerBrush, const QColor &outlineColor) : _brush(innerBrush) { setOutline(outlineColor); }
     explicit LayeredColorHelper(Qt::BrushStyle brushStyle, const QColor &outlineColor) : _brush(brushStyle) { setOutline(outlineColor); }
+    explicit LayeredColorHelper(const QBrush &innerBrush, Qt::GlobalColor outlineColor) : _brush(innerBrush) { setOutline(outlineColor); }
+    explicit LayeredColorHelper(Qt::BrushStyle brushStyle, Qt::GlobalColor outlineColor) : _brush(brushStyle) { setOutline(outlineColor); }
     const QBrush &brush() const { return _brush; }
     const QPen &outline() const { return _outline; }
     void setOutline(const QColor &outlineColor);
+    void setOutline(Qt::GlobalColor outlineColor);
     void setBrush(const QColor &innerColor);
+private:
+    template<class ColorType>
+    void setOutlineGeneric(const ColorType &outlineColor);
 };
 
 #endif // LAYEREDCOLORHELPER_H
